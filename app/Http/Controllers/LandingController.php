577d2d3d2ece46ab78677452099eb235765de940
Gamaliel Widhi Pradana <board.dabil@gmail.com>
2023-01-20 13:17:05 +0700

    Add Landing Middleware

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('landing');
+    }
+    
     public function index()
     {
         return view('auth.login');
