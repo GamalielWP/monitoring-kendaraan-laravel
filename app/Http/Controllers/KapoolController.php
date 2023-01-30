7dc09b8771521c07994267f411a25d313b0a77da
Gamaliel Widhi Pradana <board.dabil@gmail.com>
2023-01-30 23:26:29 +0700

    Kapool Update

--- app/Http/Controllers/KapoolController.php
+++ app/Http/Controllers/KapoolController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\StatusPesanan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class KapoolController extends Controller
 {
@@ -13,6 +15,26 @@ class KapoolController extends Controller
 
     public function index()
     {
-        return view('kapool.index');
+        $kapoolId = Auth::user()->id;
+        
+        $allPesanan = StatusPesanan::where('id_user', $kapoolId)->get();
+
+        return view('kapool.index', compact('allPesanan', 'kapoolId'));
+    }
+
+    public function updatePesanan($id)
+    {
+        $kapoolId = Auth::user()->id;
+        
+        $statusPesanan = StatusPesanan::where('id_user', $kapoolId)->where('id_pesanan', $id);
+        $checkPesanan = StatusPesanan::where('id_pesanan', $id)->where('level', 'Kepala Tambang')->where('status', 'disetujui')->first();
+
+        if ($checkPesanan) {
+            $statusPesanan->update([
+                'status' => 'disetujui'
+            ]);
+        }
+
+       return back();
     }
 }
--- resources/views/kapool/index.blade.php
+++ resources/views/kapool/index.blade.php
@@ -14,10 +14,6 @@
                                     <option value="25">25</option>
                                 </select>
                             </div>
-                            <div class="col-auto">
-                                <a href="" class="btn btn-success" data-bs-toggle="modal"
-                                    data-bs-target="#addModal">Buat Pesanan</a>
-                            </div>
                         </div>
                     </div>
                     <div class="col-md-4">
@@ -31,13 +27,13 @@
             <div class="col-md-8">
                 @foreach ($allPesanan as $index => $ps)
                     
-                <div id="card-{{$ps->id}}" class="card mb-3">
+                <div id="card-{{$ps->pesanan->id}}" class="card mb-3">
 
                     <div class="card-body">
                         <div class="card-title row justify-content-between">
                             <div class="col-auto">
-                                <h6 class="fs-5 fw-bold">Kode Kendaraan : {{$ps->kendaraan->kode_kendaraan}}</h6>
-                                <span><b>Driver :</b> {{$ps->driver->nama_driver}}</span>
+                                <h6 class="fs-5 fw-bold">Kode Kendaraan : {{$ps->pesanan->kendaraan->kode_kendaraan}}</h6>
+                                <span><b>Driver :</b> {{$ps->pesanan->driver->nama_driver}}</span>
                             </div>
 
                             <div class="col-auto">
@@ -45,7 +41,7 @@
                             </div>
 
                             <div class="col-auto">
-                                <span class="float-right badge bg-dark fs-6">Angkutan {{$ps->kendaraan->jenis_kendaraan}}</span>
+                                <span class="float-right badge bg-dark fs-6">Angkutan {{$ps->pesanan->kendaraan->jenis_kendaraan}}</span>
                                 <div class="accordion accordion-flush" id="accordionFlush-{{$index}}">
                                     <div class="accordion-item">
                                         <h2 class="accordion-header" id="flush-headingOne-{{$index}}">
@@ -59,37 +55,34 @@
 
                         <div id="flush-collapse-{{$index}}" class="accordion-collapse collapse" data-bs-parent="#accordionFlush-{{$index}}">
                             <hr class="mt-0">
-                            <div class="row justify-content-center">
-                                <div class="col">
+                            <div class="row justify-content-between align-items-end">
+                                <div class="col-auto">
                                     <table>
                                         <tr>
                                             <td>Lama Pemakaian&ensp;</td>
-                                            <td>: {{$ps->lama_pemakaian}} Jam</td>
+                                            <td>: {{$ps->pesanan->lama_pemakaian}} Jam</td>
                                         </tr>
                                         <tr>
                                             <td>Jumlah BBM&ensp;</td>
-                                            <td>: {{$ps->jumlah_bbm}} L</td>
+                                            <td>: {{$ps->pesanan->jumlah_bbm}} L</td>
                                         </tr>
                                         <tr>
                                             <td>Tgl Pakai&ensp;</td>
-                                            <td>: {{$ps->tgl_pakai}}</td>
+                                            <td>: {{$ps->pesanan->tgl_pakai}}</td>
                                         </tr>
                                         <tr>
                                             <td>Tgl Selesai&ensp;</td>
-                                            <td>: {{$ps->tgl_selesai}}</td>
+                                            <td>: {{$ps->pesanan->tgl_selesai}}</td>
                                         </tr>
                                     </table>
                                 </div>
 
-                                <div class="col text-center">
-                                    <div class="row justify-content-center">
-                                        <div id="lv-1-card-{{$ps->id}}" class="col">
-                                            
-                                        </div>
-                                        <div id="lv-2-card-{{$ps->id}}" class="col">
-                                            
-                                        </div>
-                                    </div>
+                                <div class="col-auto">
+                                    <form action="/kapool-dashboard/update-{{$ps->pesanan->id}}" method="post">
+                                        @csrf
+                                        @method('PATCH')
+                                        <button class="btn btn-success" type="submit" {{ $ps->status == 'disetujui' ? 'disabled' : '' }}>Setujui</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -119,84 +112,6 @@
                             </li>
                         </ul>
                     </nav>
-                </div>
-            </div>
-        </div>
-
-        <!-- Modal -->
-        <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="addModalLabel">Buat Pesanan</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <form action="/admin-dashboard/pesan" method="post">
-                        @csrf
-                        @method('POST')
-                        <div class="modal-body">
-                            <div class="row g-3 mb-3">
-                                <div class="col">
-                                    <label for="kendaraan" class="form-label">Kendaraan</label>
-                                    <select id="kendaraan" name="kendaraan" class="form-select">
-                                        <option value="null">Pilih Kendaraan</option>
-                                        @foreach ($allKendaraan as $kd)
-                                            <option value="{{$kd->id}}">{{$kd->kode_kendaraan}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="col">
-                                    <label for="pakai" class="form-label">Tanggal Pakai</label>
-                                    <input id="pakai" name="pakai" type="date" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row g-3 mb-3">
-                                <div class="col">
-                                    <label for="driver" class="form-label">Driver</label>
-                                    <select id="driver" name="driver" class="form-select">
-                                        <option value="null">Pilih Driver</option>
-                                        @foreach ($allDriver as $dr)
-                                            <option value="{{$dr->id}}">{{$dr->nama_driver}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="col">
-                                    <label for="selesai" class="form-label">Tanggal Selesai</label>
-                                    <input id="selesai" name="selesai" type="date" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row g-3 mb-3">
-                                <div class="col">
-                                    <label for="lamaPakai" class="form-label">Lama Pakai</label>
-                                    <input id="lamaPakai" name="lamaPakai" type="number" class="form-control"
-                                        placeholder="Lama Pemakaian">
-                                </div>
-                                <div class="col">
-                                    <label for="katambang" class="form-label">Kepala Tambang</label>
-                                    <select id="katambang" name="katambang" class="form-select">
-                                        <option value="null">Pilih Kepala Tambang</option>
-                                        @foreach ($allKatambang as $kt)
-                                            <option value="{{$kt->id}}">{{$kt->username}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row g-3">
-                                <div class="col">
-                                    <label for="bbm" class="form-label">BBM</label>
-                                    <input id="bbm" name="bbm" type="number" class="form-control"
-                                        placeholder="Jumlah BBM">
-                                </div>
-                                <div class="col">
-                                    {{-- <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"> --}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                            <button type="submit" class="btn btn-success">Buat</button>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
